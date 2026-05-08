b1ecfffe1b33600433a4cbfdecf6719d3f0f1970
Paulo Mucci09 <paulomucci7@gmail.com>
2026-05-08 20:57:50 -0300

    CuboSexta

--- Cubo/Assets/Script/GameManager.cs
+++ Cubo/Assets/Script/GameManager.cs
@@ -9,14 +9,32 @@ public class GameManager : MonoBehaviour
     public float spawnY = 11f;
     public float spawnX = 7f;
 
+    void Start()
+    {
+        StartCoroutine(SpawnObstacle());
+    }
+
 
     private IEnumerator SpawnObstacle()
     {
         while(!isGameOver)
         {
-            var xPosition = Random.Range(-spawnX, spawnX);
 
-            Instantiate(obstaclePrefab, new Vector3(xPosition, spawnY, 0), Quaternion.identity);
+            var obstacleSpawn = Random.Range(1, 4);
+
+            for (int i = 0; i < obstacleSpawn; i ++)
+            {
+
+
+                var xPosition = Random.Range(-spawnX, spawnX);
+
+                var damping = Random.Range(0f, 2f);
+
+                var objObstacle =
+              Instantiate(obstaclePrefab, new Vector3(xPosition, spawnY, 0), Quaternion.identity);
+
+                objObstacle.GetComponent<Rigidbody>().linearDamping = damping;
+            }
 
             yield return new WaitForSeconds(spawnInterval);
 
